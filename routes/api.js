e495f452c3144d29e97e8a5407f34fb82c2eb827
Preetham Ananthkumar <preetham.mervin@gmail.com>
2024-07-18 11:21:09 +0000

    Added MongoDB database model and schema

--- routes/api.js
+++ routes/api.js
@@ -5,6 +5,8 @@ let mongodb = require("mongodb");
 let mongoose = require("mongoose");
 
 module.exports = function (app) {
+
+  // MongoDB database connection string
   let uri =
     "mongodb+srv://" +
     process.env.USER +
@@ -16,7 +18,18 @@ module.exports = function (app) {
     "?retryWrites=true&w=majority&appName=" +
     process.env.APP_NAME;
 
+  // Connect to the MongoDB database
   mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+  // Schema
+  let stockSchema = new mongoose.Schema({
+    name: {type: String, required: true},
+    likes: {type: Number, default: 0},,
+    ips: [String]
+  });
+
+  // Model
+  let Stock = mongoose.model("Stock", stockSchema);
 
   app.route("/api/stock-prices").get(function (req, res) {});
 };

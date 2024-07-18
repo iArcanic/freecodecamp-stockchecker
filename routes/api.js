b04922b3688da4033e5de034b11cb87c45446098
Preetham Ananthkumar <preetham.mervin@gmail.com>
2024-07-18 11:17:03 +0000

    Added MongoDB database connection

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,22 @@
-'use strict';
+"use strict";
+
+var expect = require("chai").expect;
+let mongodb = require("mongodb");
+let mongoose = require("mongoose");
 
 module.exports = function (app) {
+  let uri =
+    "mongodb+srv://" +
+    process.env.USER +
+    ":" +
+    process.env.PW +
+    "@" +
+    process.env.CLUSTER +
+    process.env.DB +
+    "?retryWrites=true&w=majority&appName=" +
+    process.env.APP_NAME;
 
-  app.route('/api/stock-prices')
-    .get(function (req, res){
-      
-    });
-    
+  mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+  app.route("/api/stock-prices").get(function (req, res) {});
 };
